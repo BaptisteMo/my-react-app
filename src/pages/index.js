97c282b5da850076ceca50bc1007c0acf9c13c86
Baptiste Morillon <morillon.bapt@gmail.com>
2018-11-26 23:17:30 +0100

    trying again

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ const IndexPage = () => (
   <div>
     <h1>Hi people</h1>
     <p>Let's begin a new web app</p>
-    <p>Now go build something great.</p>
+    <p>Now go build something great. trying s:kdjbc:ksdjbv</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
